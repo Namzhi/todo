7b9a3a19eae6b9d67a8e4220b8b2e143c1171ed8
Margarita Naidanova <rinema@mail.ru>
2024-09-02 16:25:01 +0700

    add timer

--- src/add-item/add-item.js
+++ src/add-item/add-item.js
@@ -19,13 +19,16 @@ export default class AddItem extends Component {
 
   render() {
     return (
-      <form onSubmit={this.onSubmit}>
+      <form className="new-todo-form" onSubmit={this.onSubmit}>
         <input
           className="new-todo"
-          placeholder="What needs to be done?"
+          placeholder="Task"
           onChange={this.onChangeText}
           value={this.state.label}
+          autoFocus
         />
+        {/* <input className="new-todo-form__timer" placeholder="Min" autoFocus />
+        <input className="new-todo-form__timer" placeholder="Sec" autoFocus /> */}
       </form>
     )
   }
--- src/task/task.js
+++ src/task/task.js
@@ -3,36 +3,56 @@ import {Component} from 'react'
 import NewTaskForm from '../new-task-form'
 
 export default class Task extends Component {
+  state = {
+    timer: null,
+  }
   componentDidMount() {
     this.intervalId = setInterval(() => {
       this.setState({})
-    }, 60000)
+    }, 1000)
   }
 
+  onPlay = as => {
+    this.setState({
+      timer: new Date(),
+    })
+    console.log(as)
+  }
+  onPause = () => {
+    this.setState({
+      timer: null,
+    })
+  }
   componentWillUnmount() {
     clearInterval(this.intervalId)
   }
 
   render() {
-    const {item, onDeleted, id} = this.props
+    const {item, onDeleted, id, onToggleCompleted} = this.props
     const {formatDistanceToNowStrict} = require('date-fns')
-
+    const {timer} = this.state
     let classNames = `${item.classLi} todo-list`
     if (item.done) {
       classNames += ' completed label '
     } else {
       classNames += ' active label'
     }
+    const time = timer === null ? null : formatDistanceToNowStrict(timer, {unit: 'second'})
 
     return (
       <li className={classNames} key={id}>
         <div className="view">
           <input className="toggle" type="checkbox" checked={item.done} readOnly />
           <label>
-            <span className="description" onClick={this.props.onToggleCompleted}>
+            <span className="title" onClick={onToggleCompleted}>
               {item.description}
             </span>
-            <span className="created">{formatDistanceToNowStrict(item.dateCreated, {unit: 'minute'})}</span>
+            <span className="description">
+              <span className="timer">{time}</span>
+              <button className="icon icon-play" onClick={this.onPlay}></button>
+              <button className="icon icon-pause" onClick={this.onPause}></button>
+            </span>
+            <span className="created description">{`created ${formatDistanceToNowStrict(item.dateCreated, {unit: 'minute'})} ago`}</span>
           </label>
           <button className="icon icon-edit"></button>
           <button className="icon icon-destroy" onClick={onDeleted}></button>
